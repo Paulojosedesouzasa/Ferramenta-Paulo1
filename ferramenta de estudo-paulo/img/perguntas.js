3d05ed97350a7f8af710b41a02c16bcc971b11eb
Paulojosedesouzasa <jopauo27@gmail.com>
2024-11-18 22:00:35 -0300

    Add files via upload

---
+++ ferramenta de estudo-paulo/img/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'qual a capital do brasil ?',
+    'a capital é brasília'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França ?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é capital de portugal ?',
+    'a capital é lisboa'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital da espanha ?',
+    'a capital é madri'
+)
